0abfba0115d5de21f68253b956c2116865ddedc7
Kent Richards <kentrichardsmrhs@gmail.com>
2019-11-02 19:01:42 -0300

    Working version pre-clean up

--- src/dev/kentrichards/DictationDiff.java
+++ src/dev/kentrichards/DictationDiff.java
@@ -2,10 +2,8 @@ package dev.kentrichards;
 
 import name.fraser.neil.plaintext.diff_match_patch;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.awt.*;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -17,55 +15,81 @@ import java.util.Scanner;
 public class DictationDiff {
 
     public static void main(String[] args) {
-        switch (args.length) {
-            case 2:
-                // call function
-                System.out.println("Two args");
-                break;
-            case 3:
-                String original = readFile(args[0], false).toString(),
-                       revision = readFile(args[1], false).toString();
-
-                createVisualDiff(original, revision);
-                break;
-            default:
-                // error
-                System.exit(1);
+        if (args.length != 2 && args.length != 3) {
+            // error
+            System.exit(1);
         }
+
+        ArrayList<String> original = readFile(args[0]),
+                          revision = readFile(args[1]);
+
+        String version;
+        if (args.length == 3) {
+            version = args[2];
+        } else {
+            version = "No version specified.";
+        }
+
+        createVisualDiff(original.get(0), revision, version);
     }
 
-    private static void createVisualDiff(String original, String revision) {
+    private static void createVisualDiff(String original, ArrayList<String> revisions, String version) {
         diff_match_patch dmp = new diff_match_patch();
-        LinkedList<diff_match_patch.Diff> diff = dmp.diff_main(original, revision);
-        dmp.diff_cleanupSemantic(diff);
+        String timeStamp = ZonedDateTime.now(ZoneId.of("Canada/Atlantic")).format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        String fileName = timeStamp.replaceAll("[/ :]", ".") + ".html";
 
-        String timeStamp = ZonedDateTime.now(ZoneId.of("Canada/Atlantic")).format(DateTimeFormatter.ofPattern("yyyy.MM.dd.HH.mm.ss"));
+        LinkedList<diff_match_patch.Diff> diff;
+        try (PrintWriter writer = new PrintWriter("dictations/output/" + fileName, StandardCharsets.UTF_8)) {
+            writer.println("<b>" + timeStamp + "</b><br>Talkatoo Version: " + version + "<br><br>");
+            writer.println("<b>ORIGINAL FILE:</b><br>" + original + "<br><br>");
 
-        try (PrintWriter writer = new PrintWriter("dictations/output/" + timeStamp + ".html", StandardCharsets.UTF_8)) {
-            writer.println(dmp.diff_prettyHtml(diff));
+            int index = 1, numInserts = 0, numDeletes = 0;
+            for (String revision : revisions) {
+                diff = dmp.diff_main(original, revision);
+                dmp.diff_cleanupSemantic(diff);
+
+                for (diff_match_patch.Diff item : diff) {
+                    switch (item.operation) {
+                        case INSERT:
+                            numInserts++;
+                            break;
+                        case DELETE:
+                            numDeletes++;
+                            break;
+                    }
+                }
+
+                writer.println("<b>REVISION " + index + " DIFF (Insertions: " + numInserts + ", Deletions: " + numDeletes + "):</b><br>");
+                writer.println(dmp.diff_prettyHtml(diff) + "<br><br>");
+                numInserts = 0;
+                numDeletes = 0;
+                index++;
+            }
+
+            File htmlFile = new File("dictations/output/" + fileName);
+            Desktop.getDesktop().browse(htmlFile.toURI());
         } catch (IOException e) {
             // error
             System.exit(1);
         }
     }
 
-    private static Object readFile(String file, boolean multiline) {
+    private static ArrayList<String> readFile(String file) {
         // Automatically closes Scanner
         try (Scanner in = new Scanner(new FileReader("dictations/input/" + file))) {
-
-            if (multiline) {
-                ArrayList<String> out = new ArrayList<>();
-                while (in.hasNextLine()) {
-                    out.add(in.nextLine());
+            ArrayList<String> out = new ArrayList<>();
+            String temp;
+            while (in.hasNextLine()) {
+                temp = in.nextLine();
+                if (temp.length() > 0) {
+                    out.add(temp);
                 }
-                return out;
-            } else {
-                return in.nextLine();
             }
+            return out;
         } catch (FileNotFoundException e) {
-            // error
+            // temp
             System.exit(1);
-            return 1;
+            return new ArrayList<String>();
         }
     }
 }
