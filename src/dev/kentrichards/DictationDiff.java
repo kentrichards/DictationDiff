967b0c96463290f678e59c96288623034d559b2b
Kent Richards <kentrichardsmrhs@gmail.com>
2019-11-01 21:08:46 -0300

    Read file function implemented

--- src/dev/kentrichards/DictationDiff.java
+++ src/dev/kentrichards/DictationDiff.java
@@ -1,8 +1,54 @@
 package dev.kentrichards;
 
+import name.fraser.neil.plaintext.diff_match_patch;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class DictationDiff {
 
     public static void main(String[] args) {
-        // code goes here
+        switch (args.length) {
+            case 2:
+                // call function
+                System.out.println("Two args");
+                break;
+            case 3:
+                String original = readFile(args[0], false).toString(),
+                       revision = readFile(args[1], true).toString();
+
+                createVisualDiff(original, revision);
+                break;
+            default:
+                // error
+                System.exit(1);
+        }
+    }
+
+    private static void createVisualDiff(String original, String revision) {
+
+    }
+
+    private static Object readFile(String file, boolean multiline) {
+        // Automatically closes Scanner
+        try (Scanner in = new Scanner(new FileReader("dictations/" + file))) {
+
+            if (multiline) {
+                ArrayList<String> out = new ArrayList<>();
+                while (in.hasNextLine()) {
+                    out.add(in.nextLine());
+                }
+                return out;
+            } else {
+                return in.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            // error
+            System.exit(1);
+            return 1;
+        }
     }
 }
