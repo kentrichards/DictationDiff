90bb76c3b19a47ff69573e98df9789843f313761
Kent Richards <kentrichardsmrhs@gmail.com>
2019-11-01 21:35:47 -0300

    Visual diff functional

--- src/dev/kentrichards/DictationDiff.java
+++ src/dev/kentrichards/DictationDiff.java
@@ -4,6 +4,12 @@ import name.fraser.neil.plaintext.diff_match_patch;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -18,7 +24,7 @@ public class DictationDiff {
                 break;
             case 3:
                 String original = readFile(args[0], false).toString(),
-                       revision = readFile(args[1], true).toString();
+                       revision = readFile(args[1], false).toString();
 
                 createVisualDiff(original, revision);
                 break;
@@ -29,12 +35,23 @@ public class DictationDiff {
     }
 
     private static void createVisualDiff(String original, String revision) {
+        diff_match_patch dmp = new diff_match_patch();
+        LinkedList<diff_match_patch.Diff> diff = dmp.diff_main(original, revision);
+        dmp.diff_cleanupSemantic(diff);
 
+        String timeStamp = ZonedDateTime.now(ZoneId.of("Canada/Atlantic")).format(DateTimeFormatter.ofPattern("yyyy.MM.dd.HH.mm.ss"));
+
+        try (PrintWriter writer = new PrintWriter("dictations/output/" + timeStamp + ".html", StandardCharsets.UTF_8)) {
+            writer.println(dmp.diff_prettyHtml(diff));
+        } catch (IOException e) {
+            // error
+            System.exit(1);
+        }
     }
 
     private static Object readFile(String file, boolean multiline) {
         // Automatically closes Scanner
-        try (Scanner in = new Scanner(new FileReader("dictations/" + file))) {
+        try (Scanner in = new Scanner(new FileReader("dictations/input/" + file))) {
 
             if (multiline) {
                 ArrayList<String> out = new ArrayList<>();
